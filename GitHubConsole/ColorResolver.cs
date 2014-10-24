e7193edbfea39d887b3ee50fd3416cc197d180b2
deaddog <mikkelslsl@gmail.com>
2014-10-24 13:07:53 +0200

    Implemented color conversion and comparison methods in ColorResolver.

--- GitHubConsole/ColorResolver.cs
+++ GitHubConsole/ColorResolver.cs
@@ -9,5 +9,55 @@ namespace GitHubConsole
 {
     public static class ColorResolver
     {
+        public static ConsoleColor GetConsoleColor(string color)
+        {
+            return GetConsoleColor(ColorTranslator.FromHtml("#ff" + color));
+        }
+        public static ConsoleColor GetConsoleColor(Color color)
+        {
+            double closest = double.PositiveInfinity;
+            ConsoleColor res = ConsoleColor.Gray;
+
+            foreach(var c in consoleColors)
+            {
+                double dist = manhattanDistance(getColor(c), color);
+                if (dist < closest)
+                {
+                    closest = dist;
+                    res = c;
+                }
+            }
+
+            return res;
+        }
+
+        private static IEnumerable<ConsoleColor> consoleColors
+        {
+            get
+            {
+                foreach (ConsoleColor c in Enum.GetValues(typeof(ConsoleColor)))
+                {
+                    switch (c)
+                    {
+                        case ConsoleColor.Black:
+                        case ConsoleColor.DarkGray:
+                        case ConsoleColor.Gray:
+                        case ConsoleColor.White:
+                            continue;
+                    }
+                    yield return c;
+                }
+            }
+        }
+
+        private static Color getColor(ConsoleColor color)
+        {
+            return Color.FromName(color.ToString());
+        }
+
+        private static double manhattanDistance(Color a, Color b)
+        {
+            return Math.Abs(a.A - b.A) + Math.Abs(a.R - b.R) + Math.Abs(a.G - b.G) + Math.Abs(a.B - b.G);
+        }
     }
 }
