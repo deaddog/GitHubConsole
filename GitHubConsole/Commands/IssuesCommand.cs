c57b1e4d9b162e1bbce6aa524482566caf4b5bfe
deaddog <mikkelslsl@gmail.com>
2014-10-25 08:50:47 +0200

    Created a IssuesCommand class.

---
+++ GitHubConsole/Commands/IssuesCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubConsole.Commands
+{
+    public class IssuesCommand : Command
+    {
+        public override void Run(ArgumentDictionary args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
