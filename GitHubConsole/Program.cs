6d10cec189169c0431c96f574b34b02ef490ec19
deaddog <mikkelslsl@gmail.com>
2014-10-25 08:50:02 +0200

    Program lists the available commands and switches to the right one.

--- GitHubConsole/Program.cs
+++ GitHubConsole/Program.cs
@@ -16,9 +16,38 @@ namespace GitHubConsole
         {
             ArgumentDictionary arguments = new ArgumentDictionary(args);
 
+            if (arguments.Count == 0)
+            {
+                Console.WriteLine("GitHubConsole");
+                Console.WriteLine();
+                Console.WriteLine("Available commands:");
+                Console.WriteLine("  cred      Manages the stored GitHub credentials");
+                Console.WriteLine("  issues    Lists the GitHub issues associated with the current repo");
+                return;
+            }
+
+            Command command = getCommand(arguments[0].Key);
+            if (command == null)
+            {
+                Console.WriteLine("Unknown command: {0}.", arguments[0].Key);
+                Console.WriteLine("Run with no command to see a list of available commands.");
+            }
+            else
+                command.Run(arguments);
 #if DEBUG
             Console.ReadKey(true);
 #endif
+        }
+
+        private static Command getCommand(string key)
+        {
+            switch (key)
+            {
+                case "cred":
+                    return new CredentialCommand();
+                default:
+                    return null;
+            }
         }
     }
 }
