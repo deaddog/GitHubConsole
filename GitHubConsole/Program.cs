37cf81a4407b1eebc2492f21fd6a242aa7376206
deaddog <mikkelslsl@gmail.com>
2014-10-25 08:30:36 +0200

    Moved findRepo, findGitHubRemote and loadCredentials to the Command class.

--- GitHubConsole/Program.cs
+++ GitHubConsole/Program.cs
@@ -6,17 +6,12 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Text.RegularExpressions;
 using CredentialManagement;
+using GitHubConsole.Commands;
 
 namespace GitHubConsole
 {
     class Program
     {
-#if DEBUG
-        public static readonly string gitDirectory = findRepo(@"C:\Users\Mikkel\Documents\Git\sw7\report\");
-#else
-        public static readonly string gitDirectory = findRepo(Directory.GetCurrentDirectory());
-#endif
-
         static void Main(string[] args)
         {
             ArgumentDictionary arguments = new ArgumentDictionary(args);
@@ -46,104 +41,6 @@ namespace GitHubConsole
 #if DEBUG
             Console.ReadKey(true);
 #endif
-        }
-
-        private static string findRepo(string directory)
-        {
-            return findRepo(new DirectoryInfo(directory));
-        }
-        private static string findRepo(DirectoryInfo directory)
-        {
-            var dirs = directory.GetDirectories(".git");
-            for (int i = 0; i < dirs.Length; i++)
-                if (dirs[i].Name == ".git")
-                    return directory.FullName;
-
-            if (directory.Parent == null)
-                return null;
-            else
-                return findRepo(directory.Parent);
-        }
-
-        private static bool findGitHubRemote(out string user, out string project)
-        {
-            var remotes = findRemotes();
-
-            for (int i = 0; i < remotes.Length; i++)
-            {
-                var m = Regex.Match(remotes[i].Item2, @"https://github.com/(?<user>[^/]+)/(?<proj>.+)\.git");
-                if (m.Success)
-                {
-                    user = m.Groups["user"].Value;
-                    project = m.Groups["proj"].Value;
-                    return true;
-                }
-            }
-
-            user = null;
-            project = null;
-            return false;
-        }
-        private static Tuple<string, string>[] findRemotes()
-        {
-            System.Diagnostics.Process p = new System.Diagnostics.Process()
-            {
-                StartInfo = new System.Diagnostics.ProcessStartInfo("git.exe", "remote -v")
-                {
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                    WorkingDirectory = gitDirectory
-                }
-            };
-            p.Start();
-            StreamReader output = p.StandardOutput;
-            p.WaitForExit();
-
-            List<Tuple<string, string>> lines = new List<Tuple<string, string>>();
-            while (!output.EndOfStream) lines.Add(Tuple.Create(output.ReadLine(), string.Empty));
-
-            p.Dispose();
-
-            for (int i = 0; i < lines.Count; i++)
-            {
-                var m = Regex.Match(lines[i].Item1, @"(?<name>[^\t]+)\t(?<url>.*) \(fetch\)");
-                if (!m.Success)
-                {
-                    lines.RemoveAt(i--);
-                    continue;
-                }
-
-                lines[i] = Tuple.Create(m.Groups["name"].Value, m.Groups["url"].Value);
-            }
-
-            return lines.ToArray();
-        }
-
-        private static Credentials loadCredentials()
-        {
-            Credential c = new Credential()
-            {
-                Target = "github"
-            };
-            if (!c.Load())
-            {
-                Console.Write("Username: ");
-                string username = Console.ReadLine();
-
-                Console.Write("Password: ");
-                string password = Console.ReadLine();
-
-                c = new CredentialManagement.Credential(username, password, "github");
-                if (!c.Save())
-                {
-                    Console.WriteLine("Unable to store credentials.");
-                    return null;
-                }
-                else
-                    return new Credentials(c.Username, c.Password);
-            }
-            else
-                return new Credentials(c.Username, c.Password);
         }
     }
 }
