c9dc09de6b80050daebed3eeabf2d02fa6cd6bb9
deaddog <mikkelslsl@gmail.com>
2014-12-13 23:11:13 +0100

    Implemented the issue labeler.

--- GitHubConsole/Commands/IssuesLabeler.cs
+++ GitHubConsole/Commands/IssuesLabeler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Octokit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +9,103 @@ namespace GitHubConsole.Commands
 {
     public class IssuesLabeler : Command
     {
+        private bool first = true;
+        private List<int> issues = new List<int>();
+        private List<string> set = new List<string>();
+        private List<string> remove = new List<string>();
+
         public override void Execute()
         {
-            throw new NotImplementedException();
+            string username, project;
+            GitHubClient client = CreateClient(out username, out project);
+            if (client == null)
+                return;
+
+            List<Label> setLabels = new List<Label>();
+            List<Label> remLabels = new List<Label>();
+
+            var labels = client.Issue.Labels.GetForRepository(username, project).Result;
+            foreach (var s in set)
+            {
+                var l = labels.FirstOrDefault(x => x.Name == s);
+                if (l != null) setLabels.Add(l);
+            }
+            foreach (var r in remove)
+            {
+                var l = labels.FirstOrDefault(x => x.Name == r);
+                if (l != null) remLabels.Add(l);
+            }
+
+            foreach (var number in issues)
+            {
+                var issue = client.Issue.Get(username, project, number).Result;
+                if (issue == null)
+                {
+                    "Unknown issue [[:DarkRed:#{0}]].".ToConsoleLine(number);
+                    continue;
+                }
+
+                var update = issue.ToUpdate();
+                update.Assignee = issue.Assignee.Login;
+
+                foreach (var l in setLabels)
+                    if (update.Labels != null && !update.Labels.Contains(l.Name))
+                        update.AddLabel(l.Name);
+
+                if (update.Labels != null)
+                    foreach (var l in remLabels)
+                        update.Labels.Remove(l.Name);
+
+                client.Issue.Update(username, project, number, update).Wait();
+            }
         }
 
         public override bool HandleArgument(ArgumentStack.Argument argument)
         {
-            return base.HandleArgument(argument);
+            if (first)
+            {
+                first = false;
+                if (argument.Key != "label")
+                    return base.HandleArgument(argument);
+                return true;
+            }
+
+            int number;
+            if (int.TryParse(argument.Key, out number))
+            {
+                if (number < 0)
+                {
+                    Console.WriteLine("Issues # cannot be negative.");
+                    return false;
+                }
+                else
+                {
+                    issues.Add(number);
+                    return true;
+                }
+            }
+            else if (argument.Key == "-set")
+            {
+                for (int i = 0; i < argument.Count; i++)
+                {
+                    set.Add(argument[i]);
+                    if (argument[i].Contains('_'))
+                        set.Add(argument[i].Replace('_', ' '));
+                }
+                return true;
+            }
+            else if (argument.Key == "-remove")
+            {
+                for (int i = 0; i < argument.Count; i++)
+                {
+                    remove.Add(argument[i]);
+                    if (argument[i].Contains('_'))
+                        remove.Add(argument[i].Replace('_', ' '));
+                }
+                return true;
+            }
+            else
+                return base.HandleArgument(argument);
         }
     }
 }
