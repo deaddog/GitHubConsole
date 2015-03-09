c5bb64a01173fbc521fbcc90aa7da7dd4684232a
deaddog <mikkelslsl@gmail.com>
2015-03-09 19:58:02 +0100

    Added additional constructors to the SubCommand class allowing for easy setup of 1-6 sub commands.

--- GitHubConsole/Commands/SubCommand.cs
+++ GitHubConsole/Commands/SubCommand.cs
@@ -9,7 +9,8 @@ namespace GitHubConsole.Commands
 
         private Command active;
 
-        public SubCommand(Command fallback, string key1, Command command1)
+        public SubCommand(Command fallback,
+            string key1, Command command1)
         {
             this.active = null;
             this.fallback = fallback;
@@ -17,10 +18,54 @@ namespace GitHubConsole.Commands
             this.subcommands = new Dictionary<string, Command>();
             this.subcommands.Add(key1, command1);
         }
-        public SubCommand(Command fallback, string key1, Command command1, string key2, Command command2)
+
+        public SubCommand(Command fallback,
+            string key1, Command command1,
+            string key2, Command command2)
             : this(fallback, key1, command1)
         {
             this.subcommands.Add(key2, command2);
+        }
+
+        public SubCommand(Command fallback,
+            string key1, Command command1,
+            string key2, Command command2,
+            string key3, Command command3)
+            : this(fallback, key1, command1, key2, command2)
+        {
+            this.subcommands.Add(key3, command3);
+        }
+
+        public SubCommand(Command fallback, string key1, Command command1,
+            string key2, Command command2,
+            string key3, Command command3,
+            string key4, Command command4)
+            : this(fallback, key1, command1, key2, command2, key3, command3)
+        {
+            this.subcommands.Add(key4, command4);
+        }
+
+        public SubCommand(Command fallback,
+            string key1, Command command1,
+            string key2, Command command2,
+            string key3, Command command3,
+            string key4, Command command4,
+            string key5, Command command5)
+            : this(fallback, key1, command1, key2, command2, key3, command3, key4, command4)
+        {
+            this.subcommands.Add(key5, command5);
+        }
+
+        public SubCommand(Command fallback,
+            string key1, Command command1,
+            string key2, Command command2,
+            string key3, Command command3,
+            string key4, Command command4,
+            string key5, Command command5,
+            string key6, Command command6)
+            : this(fallback, key1, command1, key2, command2, key3, command3, key4, command4, key5, command5)
+        {
+            this.subcommands.Add(key6, command6);
         }
 
         public override bool HandleArgument(ArgumentStack.Argument argument)
