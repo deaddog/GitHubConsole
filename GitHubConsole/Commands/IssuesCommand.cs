853f59ae894c56b4b008a4d9b1b9f25037575c2a
deaddog <mikkelslsl@gmail.com>
2015-03-12 15:19:16 +0100

    IssuesCommand inherits the ManagedCommand class.

--- GitHubConsole/Commands/IssuesCommand.cs
+++ GitHubConsole/Commands/IssuesCommand.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace GitHubConsole.Commands
 {
-    public class IssuesCommand : Command
+    public class IssuesCommand : ManagedCommand
     {
         private bool openArgFound = false;
         private RepositoryIssueRequest request = new RepositoryIssueRequest();
@@ -43,82 +43,82 @@ namespace GitHubConsole.Commands
             listIssues(client, GitHub.Username, GitHub.Project, request);
         }
 
-        public override bool HandleArgument(Argument argument)
+        protected override IEnumerable<ArgumentHandlerPair> LoadArgumentHandlers()
         {
-            switch (argument.Key)
+            yield return new ArgumentHandlerPair("-open", handleOpen);
+            yield return new ArgumentHandlerPair("-closed", handleClosed);
+            yield return new ArgumentHandlerPair("-all", x => { request.State = ItemState.All; return true; });
+            yield return new ArgumentHandlerPair("-label", handleLabel);
+            yield return new ArgumentHandlerPair("-no-assignee", arg => { AndPredicate(x => x.Assignee == null); return true; });
+            yield return new ArgumentHandlerPair("-has-assignee", arg => { AndPredicate(x => x.Assignee != null); return true; });
+            yield return new ArgumentHandlerPair("-assignee", handleAssignee);
+        }
+
+        private bool handleOpen(Argument argument)
+        {
+            openArgFound = true;
+            if (request.State == ItemState.Closed)
+                request.State = ItemState.All;
+            return true;
+        }
+        private bool handleClosed(Argument argument)
+        {
+            if (request.State == ItemState.Open)
+                request.State = openArgFound ? ItemState.All : ItemState.Closed;
+            return true;
+        }
+
+        private bool handleLabel(Argument argument)
+        {
+
+            for (int i = 0; i < argument.Count; i++)
             {
-                case "-open":
-                    openArgFound = true;
-                    if (request.State == ItemState.Closed)
-                        request.State = ItemState.All;
-                    return true;
-                case "-closed":
-                    if (request.State == ItemState.Open)
-                        request.State = openArgFound ? ItemState.All : ItemState.Closed;
-                    return true;
-                case "-all":
-                    request.State = ItemState.All;
-                    return true;
+                var arg = argument[i];
+                var argReplace = argument[i].Replace('_', ' ');
 
-                case "-label":
-                    for (int i = 0; i < argument.Count; i++)
-                    {
-                        var arg = argument[i];
-                        var argReplace = argument[i].Replace('_', ' ');
-
-                        if (arg.StartsWith("^"))
-                        {
-                            arg = arg.Substring(1);
-                            argReplace = argReplace.Substring(1);
-                            AndPredicate(x => !x.Labels.Any(l => l.Name == arg || l.Name == argReplace));
-                        }
-                        else
-                        {
-                            if (arg != argReplace)
-                                AndPredicate(x => x.Labels.Any(l => l.Name == arg || l.Name == argReplace));
-                            else
-                                request.Labels.Add(arg);
-                        }
-                    }
-                    return true;
-
-                case "-no-assignee":
-                    AndPredicate(x => x.Assignee == null);
-                    return true;
-
-                case "-has-assignee":
-                    AndPredicate(x => x.Assignee != null);
-                    return true;
-
-                case "-assignee":
-                    if (argument.Count == 0)
-                    {
-                        Console.WriteLine("A user must be specified for the -assignee argument.");
-                        return false;
-                    }
-                    else if (argument.Count > 1)
-                    {
-                        Console.WriteLine("Only one user can be specified for the -assignee argument.");
-                        return false;
-                    }
-                    {
-                        var arg = argument[0];
-                        var argReplace = argument[0].Replace('_', ' ');
-
-                        if (arg.StartsWith("^"))
-                        {
-                            arg = arg.Substring(1);
-                            argReplace = argReplace.Substring(1);
-                            AndPredicate(x => x.Assignee == null || x.Assignee.Login != arg || x.Assignee.Login != argReplace);
-                        }
-                        else
-                            AndPredicate(x => x.Assignee != null && (x.Assignee.Login == arg || x.Assignee.Login == argReplace));
-                    }
-                    return true;
-
-                default:
-                    return base.HandleArgument(argument);
+                if (arg.StartsWith("^"))
+                {
+                    arg = arg.Substring(1);
+                    argReplace = argReplace.Substring(1);
+                    AndPredicate(x => !x.Labels.Any(l => l.Name == arg || l.Name == argReplace));
+                }
+                else
+                {
+                    if (arg != argReplace)
+                        AndPredicate(x => x.Labels.Any(l => l.Name == arg || l.Name == argReplace));
+                    else
+                        request.Labels.Add(arg);
+                }
             }
+            return true;
+        }
+
+        private bool handleAssignee(Argument argument)
+        {
+            if (argument.Count == 0)
+            {
+                Console.WriteLine("A user must be specified for the -assignee argument.");
+                return false;
+            }
+            else if (argument.Count > 1)
+            {
+                Console.WriteLine("Only one user can be specified for the -assignee argument.");
+                return false;
+            }
+            {
+                var arg = argument[0];
+                var argReplace = argument[0].Replace('_', ' ');
+
+                if (arg.StartsWith("^"))
+                {
+                    arg = arg.Substring(1);
+                    argReplace = argReplace.Substring(1);
+                    AndPredicate(x => x.Assignee == null || x.Assignee.Login != arg || x.Assignee.Login != argReplace);
+                }
+                else
+                    AndPredicate(x => x.Assignee != null && (x.Assignee.Login == arg || x.Assignee.Login == argReplace));
+            }
+            return true;
         }
 
         private void listIssues(GitHubClient client, string username, string project, RepositoryIssueRequest req)
