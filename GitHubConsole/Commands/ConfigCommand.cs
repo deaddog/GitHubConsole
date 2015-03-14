36812b40cb61c68559e433d12442022a90e51e68
deaddog <mikkelslsl@gmail.com>
2015-03-14 17:57:35 +0100

    The ConfigCommand class supports settings values in the config file.

--- GitHubConsole/Commands/ConfigCommand.cs
+++ GitHubConsole/Commands/ConfigCommand.cs
@@ -6,14 +6,30 @@ namespace GitHubConsole.Commands
 {
     public class ConfigCommand : ManagedCommand
     {
+        private Dictionary<string, string> setValues = new Dictionary<string, string>();
+
         protected override IEnumerable<ArgumentHandlerPair> LoadArgumentHandlers()
         {
-            throw new NotImplementedException();
+            yield return new ArgumentHandlerPair("--set", handleSet);
         }
 
         public override void Execute()
         {
-            throw new NotImplementedException();
+            foreach (var set in setValues)
+                Config.Default[set.Key] = set.Value;
+        }
+
+        private bool handleSet(Argument argument)
+        {
+            if(argument.Count != 2)
+            {
+                ColorConsole.ToConsoleLine("Setting config values requires exactly two arguments:");
+                ColorConsole.ToConsoleLine("  github config --set <key> <value>");
+                return false;
+            }
+
+            setValues[argument[0]] = argument[1];
+            return true;
         }
     }
 }
