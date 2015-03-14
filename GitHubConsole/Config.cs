3d07abfc06cb5609bc2fa8c4137c915bb84f8fcc
deaddog <mikkelslsl@gmail.com>
2015-03-14 16:22:46 +0100

    Implemented getting/setting of configuration values. This will be used to support #5.

--- GitHubConsole/Config.cs
+++ GitHubConsole/Config.cs
@@ -89,5 +89,58 @@ namespace GitHubConsole
                     values[temp.Item1] = temp.Item2;
             }
         }
+
+        public string this[string key]
+        {
+            get
+            {
+                key = key.ToLower();
+
+                string result = null;
+                values.TryGetValue(key, out result);
+                return result;
+            }
+            set
+            {
+                if (key == null)
+                    throw new ArgumentNullException("key");
+                if (value == null)
+                    throw new ArgumentNullException("value");
+
+                key = key.Trim().ToLower();
+                value = value.Trim();
+
+                if (key.Length == 0)
+                    return;
+
+                if (!Regex.IsMatch(key, "^[a-zA-Z0-9]+$"))
+                    return;
+                if (value.Contains('\r') || value.Contains('\n'))
+                    return;
+
+                string setting = string.Format("{0} = {1}", key, value);
+
+                if (values.ContainsKey(key))
+                {
+                    bool updated = false;
+                    var lines = File.ReadAllLines(configFilePath, Encoding.UTF8);
+                    for (int i = 0; i < lines.Length; i++)
+                        if (Regex.IsMatch(lines[i], key + " *="))
+                            if (updated)
+                                lines[i] = null;
+                            else
+                            {
+                                lines[i] = setting;
+                                updated = true;
+                            }
+
+                    File.WriteAllText(configFilePath, string.Join("\n", lines.Where(x => x != null)) + "\n", Encoding.UTF8);
+                }
+                else
+                    File.AppendAllText(configFilePath + "\n", setting, Encoding.UTF8);
+
+                values[key] = value;
+            }
+        }
     }
 }
