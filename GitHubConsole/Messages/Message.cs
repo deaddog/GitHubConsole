7cb69e03cba7f4edeba1739ebbee1d8ec8d69d91
deaddog <mikkelslsl@gmail.com>
2015-03-14 19:59:48 +0100

    Created a simple class for handling error-messaging in the console. This will replace the error-messaging method.

---
+++ GitHubConsole/Messages/Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubConsole.Messages
+{
+    public class Message
+    {
+        private string message;
+
+        public Message(string message)
+        {
+            this.message = message;
+        }
+        public Message(string message, params object[] args)
+            : this(string.Format(message, args))
+        {
+        }
+
+        public virtual string GetMessage()
+        {
+            return message;
+        }
+    }
+}
