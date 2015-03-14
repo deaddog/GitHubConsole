e8827115af7b844636018c3ab11c2c6d05402167
deaddog <mikkelslsl@gmail.com>
2015-03-14 17:30:31 +0100

    Created a command for managing the config file.

---
+++ GitHubConsole/Commands/ConfigCommand.cs
@@ -0,0 +1,19 @@
+﻿using GitHubConsole.Commands.Structure;
+using System;
+using System.Collections.Generic;
+
+namespace GitHubConsole.Commands
+{
+    public class ConfigCommand : ManagedCommand
+    {
+        protected override IEnumerable<ArgumentHandlerPair> LoadArgumentHandlers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
