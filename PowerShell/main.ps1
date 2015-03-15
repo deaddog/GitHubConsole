c5036aeea732a4e7861e3f712f34db551b7bee2a
deaddog <mikkelslsl@gmail.com>
2015-03-15 15:08:43 +0100

    Added a simple script file that will register itself for tab-expansion, using the existing function as a fallback.

---
+++ PowerShell/main.ps1
@@ -0,0 +1,28 @@
+﻿
+if (Test-Path Function:\TabExpansion) {
+  Rename-Item Function:\TabExpansion GitHubTabExpansionBackup
+}
+
+function TabExpansion($line, $lastWord) {
+  $lastBlock = [regex]::Split($line, '[|;]')[-1].TrimStart()
+
+  switch -regex ($lastBlock) {
+    "^$(Get-AliasPattern github) (.*)" { GitHubTabExpansion $lastBlock }
+
+    default { if (Test-Path Function:\GitHubTabExpansionBackup) { GitHubTabExpansionBackup $line $lastWord } }
+  }
+}
+
+function GitHubTabExpansion($lastBlock) {
+  switch -regex ($lastBlock -replace "^$(Get-AliasPattern github) ","") {
+  
+  #switch -regex ($lastBlock) {
+    # Handles git push remote <branch>
+    # Handles git pull remote <branch>
+    "^lolcode (?<lol>.+)" {
+      #Write-Host Yes
+        #gitBranches $matches['lol']
+        @(1,"Hello",3.5,"World")
+    }
+  }
+}
