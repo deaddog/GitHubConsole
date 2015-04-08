8feb157d5ab8094b6bbff965f32c52abae1c9ad5
deaddog <mikkelslsl@gmail.com>
2015-04-08 11:45:39 +0200

    Included a few additional uses of ColorConsole.ToConsoleLine that were only used in Release configuration. This applies to #18.

--- GitHubConsole/Program.cs
+++ GitHubConsole/Program.cs
@@ -20,9 +20,9 @@ namespace GitHubConsole
                 {
                     Octokit.AuthorizationException credex = aggex.InnerException as Octokit.AuthorizationException;
 
-                    ColorConsole.ToConsoleLine("GitHub responded to your request with an authentification error:");
-                    ColorConsole.ToConsoleLine("[[:Red:[{1}] {0}]]", credex.Message, credex.StatusCode);
-                    ColorConsole.ToConsoleLine("Run [[:Yellow:github config --set authtoken <token>]] to set authentification token.");
+                    ColorConsole.WriteLine("GitHub responded to your request with an authentification error:");
+                    ColorConsole.WriteLine("[[:Red:[{1}] {0}]]", credex.Message, credex.StatusCode);
+                    ColorConsole.WriteLine("Run [[:Yellow:github config --set authtoken <token>]] to set authentification token.");
                 }
                 else
                     throw aggex;
