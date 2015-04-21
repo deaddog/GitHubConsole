9e75876a54bb8078429d5e54596cfc8decf61ebd
deaddog <mikkelslsl@gmail.com>
2015-04-21 07:28:34 +0200

    Added parameters for the issues page, a single issue page, the labels page and the wiki page on github.com.

--- GitHubConsole/Commands/GoCommand.cs
+++ GitHubConsole/Commands/GoCommand.cs
@@ -4,6 +4,18 @@ namespace GitHubConsole.Commands
 {
     public class GoCommand : Command
     {
+        private readonly FlagParameter issues = null;
+        [Name("--issue", "-i")]
+        private readonly Parameter<int> issue = null;
+
+        private readonly FlagParameter labels = null;
+        private readonly FlagParameter wiki = null;
+
+        public GoCommand()
+        {
+            Validator.AddOnlyOne(issues, issue, labels, wiki);
+        }
+
         protected override void Execute()
         {
             string url = string.Format("https://github.com/{0}/{1}", GitHub.Username, GitHub.Project) + getPage();
@@ -12,6 +24,14 @@ namespace GitHubConsole.Commands
 
         private string getPage()
         {
+            if (issues.IsSet)
+                return "/issues";
+            if (issue.IsSet)
+                return "/issues/" + issue.Value;
+            if (labels.IsSet)
+                return "/labels";
+            if (wiki.IsSet)
+                return "/wiki";
             return string.Empty;
         }
     }
