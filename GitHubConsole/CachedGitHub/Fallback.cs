f488265bbcbbbacb3dcb22ec850e1e0ebea8c0b5
deaddog <mikkelslsl@gmail.com>
2015-07-23 08:44:18 +0200

    Created a simple abstract class that will handle fallback calls in all cached implementations.

---
+++ GitHubConsole/CachedGitHub/Fallback.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GitHubConsole.CachedGitHub
+{
+    public abstract class Fallback<T> where T : class
+    {
+        private T _fallback;
+        private Func<T> getFallback;
+
+        protected T fallback => _fallback ?? (_fallback = getFallback());
+
+        public Fallback(Func<T> fallback)
+        {
+            if (fallback == null)
+                throw new ArgumentNullException(nameof(fallback));
+
+            this._fallback = null;
+            this.getFallback = fallback;
+        }
+        public Fallback(T fallback)
+        {
+            if (fallback == null)
+                throw new ArgumentNullException(nameof(fallback));
+
+            this._fallback = fallback;
+            this.getFallback = null;
+        }
+    }
+}
