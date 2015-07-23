a93ee30e9d328fd6159f20a2af39fe008ba06644
deaddog <mikkelslsl@gmail.com>
2015-07-23 08:02:26 +0200

    Created an "empty" implementation of the IGitHubClient interface.

---
+++ GitHubConsole/CachedGitHub/CachedGitHubClient.cs
@@ -0,0 +1,30 @@
+﻿using Octokit;
+using System;
+
+namespace GitHubConsole.CachedGitHub
+{
+    public class CachedGitHubClient : Fallback<GitHubClient>, IGitHubClient
+    {
+        public CachedGitHubClient(GitHubClient client)
+            :base(client)
+        {
+        }
+
+        public IConnection Connection => fallback.Connection;
+        public IAuthorizationsClient Authorization => fallback.Authorization;
+        public IActivitiesClient Activity => fallback.Activity;
+        public IIssuesClient Issue => fallback.Issue;
+        public IMiscellaneousClient Miscellaneous => fallback.Miscellaneous;
+        public IOauthClient Oauth => fallback.Oauth;
+        public IOrganizationsClient Organization => fallback.Organization;
+        public IPullRequestsClient PullRequest => fallback.PullRequest;
+        public IRepositoriesClient Repository => fallback.Repository;
+        public IGistsClient Gist => fallback.Gist;
+        public IReleasesClient Release => fallback.Release;
+        public ISshKeysClient SshKey => fallback.SshKey;
+        public IUsersClient User => fallback.User;
+        public INotificationsClient Notification => fallback.Notification;
+        public IGitDatabaseClient GitDatabase => fallback.GitDatabase;
+        public ISearchClient Search => fallback.Search;
+    }
+}
