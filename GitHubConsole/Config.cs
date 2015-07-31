9b5ff59d61d77a4d6e90f2ac023cb68fedd19669
deaddog <mikkelslsl@gmail.com>
2015-07-31 15:28:56 +0200

    Declared a local and a global config file. The Config.Default property retrieves both.

--- GitHubConsole/Config.cs
+++ GitHubConsole/Config.cs
@@ -6,8 +6,13 @@ namespace GitHubConsole
 {
     public static class Config
     {
-        private static Configuration global;
+        private static Configuration local, global;
+        private static IConfiguration group;
 
-        public static Configuration Default => global ?? (global = new Configuration(Path.Combine(GitHub.GlobalStorage, "config")));
+        public static Configuration Local => local ?? (local = new Configuration(Path.Combine(GitHub.RepositoryStorage, "config")));
+        
+        public static Configuration Global => global ?? (global = new Configuration(Path.Combine(GitHub.GlobalStorage, "config")));
+
+        public static IConfiguration Default => group ?? (group = new ConfigurationGroup(Local, Global));
     }
 }
