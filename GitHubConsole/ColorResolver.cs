a11b3a73a40f5539fdc61ad8570c77b19dfae5c1
deaddog <mikkelslsl@gmail.com>
2015-08-02 15:07:11 +0200

    Provided a better definition for the console colors.

--- GitHubConsole/ColorResolver.cs
+++ GitHubConsole/ColorResolver.cs
@@ -53,7 +53,28 @@ namespace GitHubConsole
 
         private static Color getColor(ConsoleColor color)
         {
-            return Color.FromName(color.ToString());
+            switch (color)
+            {
+                case ConsoleColor.Black: return Color.FromArgb(0, 0, 0);
+                case ConsoleColor.Gray: return Color.FromArgb(192, 192, 192);
+                case ConsoleColor.DarkGray: return Color.FromArgb(128, 128, 128);
+                case ConsoleColor.White: return Color.FromArgb(255, 255, 255);
+
+                case ConsoleColor.DarkBlue: return Color.FromArgb(0, 0, 128);
+                case ConsoleColor.DarkGreen: return Color.FromArgb(0, 128, 0);
+                case ConsoleColor.DarkCyan: return Color.FromArgb(0, 128, 128);
+                case ConsoleColor.DarkRed: return Color.FromArgb(128, 0, 0);
+                case ConsoleColor.DarkMagenta: return Color.FromArgb(128, 0, 128);
+                case ConsoleColor.DarkYellow: return Color.FromArgb(128, 128, 0);
+                case ConsoleColor.Blue: return Color.FromArgb(0, 0, 255);
+                case ConsoleColor.Green: return Color.FromArgb(0, 255, 0);
+                case ConsoleColor.Cyan: return Color.FromArgb(0, 255, 255);
+                case ConsoleColor.Red: return Color.FromArgb(255, 0, 0);
+                case ConsoleColor.Magenta: return Color.FromArgb(255, 0, 255);
+                case ConsoleColor.Yellow: return Color.FromArgb(255, 255, 0);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(color));
+            }
         }
 
         private static double manhattanDistance(Color a, Color b)
