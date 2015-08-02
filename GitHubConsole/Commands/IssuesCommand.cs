a576cb3df5fb05927b85a2cbc4f9afb665837b70
deaddog <mikkelslsl@gmail.com>
2015-08-02 12:10:22 +0200

    Created a 'mine' condition that is only rendered if an issue is assigned to the user.

--- GitHubConsole/Commands/IssuesCommand.cs
+++ GitHubConsole/Commands/IssuesCommand.cs
@@ -440,6 +440,7 @@ namespace GitHubConsole.Commands
                 {
                     case "labels": return issue.Labels.Count > 0;
                     case "assignee": return issue.Assignee != null;
+                    case "mine": return issue?.Assignee?.Login == GitHub.CurrentUser?.Login;
                     case "description": return issue.Body != null && issue.Body.Length > 0;
                     default: return base.ValidateCondition(condition);
                 }
