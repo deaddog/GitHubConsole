152b427d8422c67a403cdbe7753ee78438577078
deaddog <mikkelslsl@gmail.com>
2015-08-02 13:06:11 +0200

    The paths of the two configuration files are exposed through properties in the Config class.

--- GitHubConsole/Config.cs
+++ GitHubConsole/Config.cs
@@ -9,9 +9,17 @@ namespace GitHubConsole
         private static Configuration local, global;
         private static IConfiguration group;
 
-        public static Configuration Local => local ?? (local = new Configuration(Path.Combine(GitHub.RepositoryStorage, "config")));
-        
-        public static Configuration Global => global ?? (global = new Configuration(Path.Combine(GitHub.GlobalStorage, "config")));
+        public static string LocalPath
+        {
+            get { return Path.Combine(GitHub.RepositoryStorage, "config"); }
+        }
+        public static string GlobalPath
+        {
+            get { return Path.Combine(GitHub.GlobalStorage, "config"); }
+        }
+
+        public static Configuration Local => local ?? (local = new Configuration(LocalPath));
+        public static Configuration Global => global ?? (global = new Configuration(GlobalPath));
 
         public static IConfiguration Default => group ?? (group = new ConfigurationGroup(Local, Global));
     }
