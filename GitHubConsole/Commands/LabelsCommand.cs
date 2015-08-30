f691c72504a5a5dc8124a496aa658d121836e5bc
deaddog <mikkelslsl@gmail.com>
2015-08-30 13:13:37 +0200

    The labels command lists all labels in the current repository.

--- GitHubConsole/Commands/LabelsCommand.cs
+++ GitHubConsole/Commands/LabelsCommand.cs
@@ -1,12 +1,23 @@
 ﻿using CommandLineParsing;
+using Octokit;
+using System.Linq;
 
 namespace GitHubConsole.Commands
 {
     [Description("Lists, modifies, creates and deletes github labels for this repository")]
     public class LabelsCommand : Command
     {
+        private Label[] _allLabels = null;
+        private Label[] allLabels => _allLabels ?? (_allLabels = GitHub.Client.Issue.Labels.GetAllForRepository(GitHub.Username, GitHub.Project).Result.ToArray());
+
         public LabelsCommand()
         {
+        }
+
+        protected override void Execute()
+        {
+            foreach (var l in allLabels)
+                ColorConsole.WriteLine($"[{ColorResolver.GetConsoleColor(l)}:{l.Name}]");
         }
     }
 }
