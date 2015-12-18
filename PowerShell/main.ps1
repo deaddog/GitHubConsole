7b769c98ea7f0586b78eccc20436ea6e6445a2f8
deaddog <mikkelslsl@gmail.com>
2015-12-18 21:28:04 +0100

    Added comments to most of the lines in the script.

--- PowerShell/main.ps1
+++ PowerShell/main.ps1
@@ -1,11 +1,15 @@
-﻿
+﻿# Determine if a function called TabExpansion is already defined, and if so rename it GitHubTabExpansionBackup.
 if ((Test-Path Function:\TabExpansion) -and (-not (Test-Path Function:\GitHubTabExpansionBackup))) {
   Rename-Item Function:\TabExpansion GitHubTabExpansionBackup
 }
 
+# The definition of the new tab-expansion method.
 function TabExpansion($line, $lastWord) {
+  # Get the last block in the current string by handling piping and command separation.
   $lastBlock = [regex]::Split($line, '[|;]')[-1].TrimStart()
 
+  # If the last block starts with 'github' or an alias, call the new tab expansion method.
+  # Otherwise call the tab expansion backup.
   switch -regex ($lastBlock) {
     "^$(Get-AliasPattern github) (.*)" { GitHubTabExpansion $lastBlock }
 
@@ -13,20 +17,25 @@ function TabExpansion($line, $lastWord) {
   }
 }
 
+# The definition of which type of tab expansion to perform.
 function GitHubTabExpansion($lastBlock) {
+  # Remove the 'github' or other aliasing from the block.
   switch -regex ($lastBlock -replace "^$(Get-AliasPattern github) ","") {
 
-  #switch -regex ($lastBlock) {
     # Handles github <cmd>
     "^(?<cmd>[^ ]*)$" {
         githubCommands $matches['cmd']
     }
 
+    # No tab expansion.
     default { @("") }
   }
 }
 
+# A global variable for caching the list of available github commands.
 $global:githubListOfAvailableCommands = $null
+
+# The function that handles tab expansion for github commands.
 function script:githubCommands($command)
 {
   if ($global:githubListOfAvailableCommands -eq $null) {
